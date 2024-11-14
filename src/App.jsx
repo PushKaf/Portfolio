f9c7e9d7801a66ebe49ba6dba0b12ae11626aa38
PushKaf <pushkal.kafley@gmail.com>
2024-11-13 21:12:57 -0500

    Fixed analytics... duh

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { Home } from './pages/Home';
 import { Secret } from './pages/Secret';
 import { Accessibility } from 'accessibility';
-import { Analytics } from '@vercel/analytics/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
@@ -11,7 +10,6 @@ const App = () => {
 
   return (
     <Router>
-      <Analytics />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/flowers" element={<Secret />} />
