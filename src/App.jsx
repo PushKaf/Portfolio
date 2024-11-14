e178777263adea6e0aaf2daecf92ee22dc787485
PushKaf <pushkal.kafley@gmail.com>
2024-11-13 21:05:34 -0500

    re-added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Home } from './pages/Home';
 import { Secret } from './pages/Secret';
 import { Accessibility } from 'accessibility';
+import { Analytics } from '@vercel/analytics/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
@@ -10,6 +11,7 @@ const App = () => {
 
   return (
     <Router>
+      <Analytics />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/flowers" element={<Secret />} />
