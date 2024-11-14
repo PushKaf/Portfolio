086903755038e891b2f3420dfc7e3c420ced6714
PushKaf <pushkal.kafley@gmail.com>
2024-11-13 21:47:05 -0500

    SpeedInsights should be fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { Home } from './pages/Home';
 import { Secret } from './pages/Secret';
 import { Accessibility } from 'accessibility';
-import { SpeedInsights } from "@vercel/speed-insights/react"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
@@ -11,7 +10,6 @@ const App = () => {
 
   return (
     <Router>
-    <SpeedInsights />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/flowers" element={<Secret />} />
